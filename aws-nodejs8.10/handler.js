6d6cdfba53ed76999b5721ef0a8d5d1cfc53f7df
Danny Waite <dannywaite7@gmail.com>
2018-11-14 12:31:51 +0000

    tweeks

--- aws-nodejs8.10/handler.js
+++ aws-nodejs8.10/handler.js
@@ -1,8 +1,10 @@
 const logic = require(".");
 
+if (logic.setup) logic.setup();
+
 exports.handler = function(event, context, callback) {   
     try {
-        callback(null, logic(event));
+        callback(null, logic.handler(event));
     }
     catch (e) {
         callback(e)
