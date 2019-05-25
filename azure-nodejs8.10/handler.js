d86bb791c24ac5d8cfac704284e3ad4ef78e1232
Guillem Mateos <bbguillem@gmail.com>
2019-05-25 12:07:04 +0200

    Eslint and some cleaning up

--- azure-nodejs8.10/handler.js
+++ azure-nodejs8.10/handler.js
@@ -1,11 +1,11 @@
 const logic = require('./index');
 
-module.exports = async function (context, eventInput) {
-  if (process.env.DEBUG)
-    context.log('node EventHub trigger function processed a request.', eventInput);
+module.exports = async function run(context, eventInput) {
+  if (process.env.DEBUG) {
+    context.log('EventHub trigger processed an event', eventInput);
+  }
 
   const output = await logic.handler(eventInput, context);
 
   context.done(null, output);
 };
-
--- gcp-nodejs8.10/handler.js
+++ gcp-nodejs8.10/handler.js
@@ -1,24 +1,30 @@
 // Imports the Google Cloud client library
-const {PubSub} = require('@google-cloud/pubsub');
+const { PubSub } = require('@google-cloud/pubsub');
 const index = require('./index');
 
 const pubsub = new PubSub();
 
-module.exports.process = async function (message, context) {
-    
-    const event = message.data ? JSON.parse( Buffer.from(message.data, 'base64').toString() ): {};
+module.exports.process = async function processEvent(message, context) {
+  const event = message.data ? JSON.parse(Buffer.from(message.data, 'base64').toString()) : {};
 
-    if (process.env.DEBUG) {
-        console.log(`Raw message: ${JSON.stringify(message.data)}`);
-        console.log(`Processed message: ${event}`);
+  if (process.env.DEBUG) {
+    // eslint-disable-next-line no-console
+    console.log(`Raw message: ${JSON.stringify(message.data)}`);
+    // eslint-disable-next-line no-console
+    console.log(`Processed message: ${event}`);
+  }
+
+  const out = await index.handler(event);
+
+  // Publishes the message and prints the messageID on console
+  if (process.env.STREAM_NAME && out != null) {
+    try {
+      const messageId = await pubsub.topic(process.env.STREAM_NAME).publishJSON(out);
+      // eslint-disable-next-line no-console
+      console.log(`Message ${messageId} published.`);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(new Error(e));
     }
-
-    const out = await index.handler(event);
-
-    // Publishes the message and prints the messageID on console
-    if (process.env.STREAM_NAME) { 
-        //const dataBuffer = Buffer.from(JSON.stringify(out), 'utf-8');
-        const messageId = await pubsub.topic(process.env.STREAM_NAME).publishJSON(out);
-        console.log(`Message ${messageId} published.`);
-    };
+  }
 };
