c2407c73937ced21009152221b63fc324138816a
Guillem Mateos <bbguillem@gmail.com>
2019-08-31 20:42:05 +0200

    Different input and output support

---
+++ aws-nodejs8.10/processors/kinesis/receive.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const furnaceSDK = require('@project-furnace/sdk');
+
+const funcArray = [];
+let logic;
+
+if (process.env.COMBINE) {
+  // eslint-disable-next-line global-require
+  const funcs = require('require-all')({
+    dirname: path.join(__dirname, '../..', 'combined'),
+    filter: /(index)\.js$/,
+  });
+
+  const funcOrder = process.env.COMBINE.split(',');
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const func of funcOrder) {
+    funcArray.push(funcs[func].index.handler);
+  }
+} else {
+  // eslint-disable-next-line global-require
+  logic = require('../../index');
+}
+
+async function receive(events) {
+  const outputEvents = [];
+  for (let ii = 0; ii < events.length; ii += 1) {
+    if (events[ii].kinesis && events[ii].kinesis.data) {
+      const event = JSON.parse(Buffer.from(events[ii].kinesis.data, 'base64'));
+      if (!process.env.COMBINE) {
+        // we are using a for loop that allows for async
+        // eslint-disable-next-line no-await-in-loop
+        outputEvents.push(await logic.handler(event));
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        const out = await furnaceSDK.fp.pipe(
+          ...funcArray,
+        )(event);
+        outputEvents.push(out);
+      }
+    } else if (process.env.DEBUG) {
+      // eslint-disable-next-line no-console
+      console.log('No "kinesis" or "data" properties in events received');
+    }
+  }
+  return outputEvents;
+}
+
+module.exports.receive = receive;
