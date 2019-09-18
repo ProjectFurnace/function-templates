b20417d5a7aa329dd2e59364982c42a138a43bea
Guillem Mateos <bbguillem@gmail.com>
2019-09-19 00:18:02 +0200

    SQS support on AWS template

--- aws-python3.6/handler.py
+++ aws-python3.6/handler.py
@@ -4,16 +4,24 @@ import gzip
 import base64
 import json
 import os
+import uuid
 import pkgutil
 import asyncio
 from importlib import import_module
+
+source_type = ''
+output_type = ''
+queue_url = ''
+lambda_array = []
 
 def dynamic_import(abs_module_path, class_name):
     module_object = import_module(abs_module_path)
     target_class = getattr(module_object, class_name)
     return target_class
 
+# process an event
 async def process_event(record, context):
+    # if we have a set of combined functions, run them all, otherwise just run this one
     if len(lambda_array) > 1:
         new_event = record
         for fnc in lambda_array:
@@ -24,6 +32,7 @@ async def process_event(record, context):
 
     return processed_result
 
+# specific case to process AWS logs
 def process_awslogs(event):
     data = event['awslogs']['data']
     compressed_payload = base64.b64decode(data)
@@ -36,6 +45,7 @@ def process_awslogs(event):
     log_events = payload['logEvents']
     return log_events
 
+# decode incoming kinesis data
 def process_kinesis(event):
     records = event['Records']
     events = []
@@ -47,17 +57,39 @@ def process_kinesis(event):
         events.append(json.loads(payload))
     
     return events
-        
 
+#decode incoming SQS data
+def process_sqs(event):
+    records = event['Records']
+    events = []
+    
+    for record in records:
+        data = record['body']
+        events.append(json.loads(data))
+    
+    return events
+        
+# define a passthrough for when we have no clue what an event is
 def passtrough(event):
     return event
 
+# handle events based on the structure
 def handler(event, context):
-    
+    global output_type
+    global source_type
+    global queue_url
+
     if 'awslogs' in event:
         processed_events = process_awslogs(event)
     elif 'Records' in event:
-        processed_events = process_kinesis(event)
+        if event['Records'][0]['eventSource'] == 'aws:kinesis':
+            source_type = 'aws.kinesis.stream'
+            processed_events = process_kinesis(event)
+        elif event['Records'][0]['eventSource'] == 'aws:sqs':
+            source_type = 'aws.sqs.queue'
+            processed_events = process_sqs(event)
+        else:
+            raise TypeError('Unsupported event type: ' + event['Records'][0]['eventSource'])
     else:
         processed_events = passtrough(event)
     
@@ -71,16 +103,30 @@ def handler(event, context):
     loop.close()
 
     for fut in done:
-        record = {'Data': json.dumps(fut.result()),'PartitionKey': os.environ['PARTITION_KEY']}
-        processed_records.append(record)
+        # the structure is different depending on if we output SQS or kinesis
+        # deciding between kinesis/SQS output is based on input and on whether we have OUTPUT_TYPE
+        if os.environ.get('OUTPUT_TYPE', source_type) == 'aws.kinesis.stream':
+            output_type = 'aws.kinesis.stream'
+            record = {'Data': json.dumps(fut.result()),'PartitionKey': os.environ['PARTITION_KEY']}
+            processed_records.append(record)
+        elif os.environ.get('OUTPUT_TYPE', source_type) == 'aws.sqs.queue':
+            output_type = 'aws.sqs.queue'
+            record = {'MessageBody': json.dumps(fut.result()),'Id': str(uuid.uuid4())}
+            processed_records.append(record)
+            # get also the output queue url if we don't have it yet
+            if queue_url == '':
+                queue_url = boto3.client('sqs', region_name=os.environ['AWS_REGION']).get_queue_url(QueueName=os.environ['STREAM_NAME'])['QueueUrl']
 
+    # output based on the input and on whether we have an output type defined or not
     if len(processed_records) > 0 and 'STREAM_NAME' in os.environ:
-        kinesis_client = boto3.client('kinesis', region_name=os.environ['AWS_REGION'])
-        put_response = kinesis_client.put_records(Records=processed_records, StreamName=os.environ['STREAM_NAME'])
+        if output_type == 'aws.kinesis.stream':
+            kinesis_client = boto3.client('kinesis', region_name=os.environ['AWS_REGION'])
+            put_response = kinesis_client.put_records(Records=processed_records, StreamName=os.environ['STREAM_NAME'])
+        elif output_type == 'aws.sqs.queue':
+            sqs_client = boto3.client('sqs', region_name=os.environ['AWS_REGION'])
+            put_response = sqs_client.send_message_batch(QueueUrl=queue_url, Entries=processed_records)
 
     return 'Successfully processed {} records.'.format(len(processed_events))
-
-lambda_array = []
 
 if 'COMBINED' in os.environ:
     BASE_PATH = os.path.dirname(__file__) + '/combined/'
