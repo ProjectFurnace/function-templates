98867c736d79c893ec9dad77661809df4126d7fc
Guillem Mateos <bbguillem@gmail.com>
2019-09-26 10:57:05 +0200

    Support for s3 and partial CW

--- aws-nodejs8.10/processorFactory.js
+++ aws-nodejs8.10/processorFactory.js
@@ -2,6 +2,8 @@ const kinesisReceiver = require('./processors/kinesis/receive').receive;
 const kinesisSender = require('./processors/kinesis/send').send;
 const sqsReceiver = require('./processors/sqs/receive').receive;
 const sqsSender = require('./processors/sqs/send').send;
+const s3Receiver = require('./processors/s3/receive').receive;
+const cloudwatchReceiver = require('./processors/cloudwatch/receive').receive;
 
 module.exports.createInstance = (payload, output) => {
   if (!payload.Records || !payload.Records.length > 0) throw new Error('unable to detect payload type');
@@ -14,15 +16,21 @@ module.exports.createInstance = (payload, output) => {
   }
 
   if (firstRecord.eventSource) {
+    // switch output based on either input or the outputType env var
     switch (firstRecord.eventSource) {
       case 'aws:kinesis':
         return [kinesisReceiver, (outputType === 'aws.sqs.queue' ? sqsSender : kinesisSender)];
       case 'aws:sqs':
         return [sqsReceiver, (outputType === 'aws.kinesis.stream' ? kinesisSender : sqsSender)];
+      case 'aws:s3':
+        return [s3Receiver, (outputType === 'aws.sqs.queue' ? sqsSender : kinesisSender)];
       default:
         throw new Error(`unable to get processor for eventSource ${firstRecord.eventName}`);
     }
+  // cloudwatch scheduled events have a completely different event format...
+  } else if (payload.source === 'aws.events') {
+    return [cloudwatchReceiver, (outputType === 'aws.sqs.queue' ? sqsSender : kinesisSender)];
   } else {
-    throw new Error('eventSource field not available in payload');
+    throw new Error('eventSource not available in payload records or payload source not supported');
   }
 };
