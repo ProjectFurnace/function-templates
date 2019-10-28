aa1ea3569316588e21004ed9f2c5878061a1ec10
Guillem Mateos <bbguillem@gmail.com>
2019-10-28 19:25:32 +0100

    Changes for api gateway as a source

---
+++ aws-nodejs8.10/processors/apigateway/receive.js
@@ -0,0 +1,26 @@
+const furnaceSDK = require('@project-furnace/sdk');
+const processorInitialise = require('../lib/initialise');
+
+const { logic, funcArray } = processorInitialise;
+
+const type = 'API_GATEWAY';
+
+async function receive(event) {
+  const outputEvents = [];
+  if (event && event.body) {
+    if (!process.env.COMBINE) {
+      outputEvents.push(await logic.handler(event));
+    } else {
+      const out = await furnaceSDK.fp.pipe(
+        ...funcArray,
+      )(event);
+      outputEvents.push(out);
+    }
+  } else if (process.env.DEBUG) {
+    // eslint-disable-next-line no-console
+    console.log('Invalid event format or missing body field');
+  }
+  return outputEvents;
+}
+
+module.exports = { type, receive };
